f6b28374941609448b97b2844a6107cd0c4ebf06
Richard Hildred <rhildred@gmail.com>
2025-03-20 19:52:19 +0000

    get k8s infrastructure monitoring added and add flask app for monitoring too

---
+++ flaskapp/toaster.sh
@@ -0,0 +1,1 @@
+OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED=true OTEL_EXPORTER_OTLP_ENDPOINT=localhost:4317 OTEL_RESOURCE_ATTRIBUTES=service.name=com.github.rhildred.INFO8985_microservice_instrumentation,service.version=de732a0 OTEL_EXPORTER_OTLP_INSECURE=true opentelemetry-instrument --traces_exporter otlp --metrics_exporter otlp --logs_exporter otlp python app.py
